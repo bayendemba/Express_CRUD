cc890da616b1d4fc623c7d5213a930c98d744109
bayendemba <bayendemba@gmail.com>
2018-11-22 17:22:58 +0000

    genres's route

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const genres =  require("./routes/genres");
+const app = express();
+
+// Connect to mongoDB
+mongoose.connect("mongodb://localhost/vidly", { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log("Could not connect to MongoDB..."));
+
+app.use(express.json());
+app.use("/api/genres", genres);    
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Serveur démarrer sur le port ${port}...`));
