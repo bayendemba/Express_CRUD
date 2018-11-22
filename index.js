cf88ecf4b13ab2902efa9d8ab665494d714d171f
bayendemba <bayendemba@gmail.com>
2018-11-22 19:43:01 +0000

    rentals

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const genres =  require("./routes/genres");
 const customers =  require("./routes/customers");
 const movies =  require("./routes/movies");
+const rentals =  require("./routes/rentals");
 const app = express();
 
 // Connect to mongoDB
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/genres", genres);    
 app.use("/api/customers", customers);    
 app.use("/api/movies", movies);    
+app.use('/api/rentals', rentals);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Serveur démarrer sur le port ${port}...`));
