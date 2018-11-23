dc18fd219af827fa1c5896059a1d5be1f422c0bb
bayendemba <bayendemba@gmail.com>
2018-11-23 13:34:16 +0000

    register user

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const genres =  require("./routes/genres");
 const customers =  require("./routes/customers");
 const movies =  require("./routes/movies");
 const rentals =  require("./routes/rentals");
+const users =  require("./routes/users");
 const app = express();
 
 // Connect to mongoDB
@@ -18,6 +19,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);    
 app.use("/api/movies", movies);    
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Serveur démarrer sur le port ${port}...`));
