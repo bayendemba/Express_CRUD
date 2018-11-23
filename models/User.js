35a5dbe067138165d5f411a173c1c5f273a2a08f
bayendemba <bayendemba@gmail.com>
2018-11-23 17:42:13 +0000

    Authorization

--- models/User.js
+++ models/User.js
@@ -22,12 +22,18 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 5,
     maxlength: 1024
-  }
+  },
+  isAdmin: Boolean  
+  // roles: [], 
+  // operations: [] 
 });
 
 // ici on ne peut pas utiliser arrow function car il n'y pas (this)
-userSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({
+    _id: this._id,
+    isAdmin: this.isAdmin
+  }, config.get("jwtPrivateKey"));
   return token;
 }
 
