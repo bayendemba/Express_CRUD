c49d817433c55f41735129f98215d628d55e1892
bayendemba <bayendemba@gmail.com>
2018-11-24 16:29:21 +0000

    add Console transport

--- startup/logging.js
+++ startup/logging.js
@@ -3,10 +3,9 @@ require("winston-mongodb");
 require("express-async-errors");
 
 module.exports = function () {
-  winston.handleExceptions(new winston.transports.File({
-    filename: "uncaughtExceptions.log"
-  }));
-
+  winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: "uncaughtExceptions.log" }));    
   process.on("unhandledRejection", (ex) => {
     throw ex;
   });
