616e722732a09cf75c4b2bdffc55a9c67a4604f5
bayendemba <bayendemba@gmail.com>
2018-11-26 18:10:51 +0000

    testing the happy path

--- routes/genres.js
+++ routes/genres.js
@@ -1,10 +1,7 @@
 const validateObjectId = require("../middleware/validateObjectId");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
-const {
-  Genre,
-  validate
-} = require("../models/Genre");
+const { Genre, validate } = require("../models/Genre");
 const express = require("express");
 const router = express.Router();
 
@@ -15,30 +12,20 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", auth, async (req, res) => {
-  const {
-    error
-  } = validate(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({
-    name: req.body.name
-  });
+  let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
 
   res.send(genre);
 });
 
 router.put("/:id", auth, async (req, res) => {
-  const {
-    error
-  } = validate(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genre.findByIdAndUpdate(req.params.id, {
-    name: req.body.name
-  }, {
-    new: true
-  });
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
 
   if (!genre) return res.status(404).send("The genre with the given Id is not found");
 
