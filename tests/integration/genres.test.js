ea6d8e522e0eb5a9395cb24c2145818d533bb47f
bayendemba <bayendemba@gmail.com>
2018-11-26 18:00:16 +0000

    testing invalid input

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,7 +1,6 @@
 const request = require("supertest");
-const {
-  Genre
-} = require("../../models/Genre");
+const { Genre } = require("../../models/Genre");
+const { User } = require("../../models/User");
 
 let server;
 
@@ -58,6 +57,30 @@ describe("/api/genres", () => {
         .send({ name: "genre1" });
 
       expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is less than 5 characters", async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "1234" });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre is more than 50 characters", async () => {
+      const token = new User().generateAuthToken();
+
+      const name = new Array(52).join("a");
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: name });
+
+      expect(res.status).toBe(400);
     });
   });
 });
