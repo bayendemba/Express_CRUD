8d00d2a86980521aaa8d6fd1b2b935a8c4a38489
bayendemba <bayendemba@gmail.com>
2018-11-27 18:26:09 +0000

    TDD RentalFee

--- routes/returns.js
+++ routes/returns.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const { Rental } = require("../models/Rental");
 const auth = require("../middleware/auth");
 const express = require("express");
@@ -16,7 +17,10 @@ router.post("/", auth, async (req, res) => {
   if(rental.dateReturned) return res.status(400).send("Return already processed.");
 
   rental.dateReturned = new Date();
+  const rentalDays = moment().diff(rental.dateOut, "days");
+  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
   await rental.save();
+
   return res.status(200).send();
 });
 
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const { Rental } = require("../../models/Rental");
 const { User } = require("../../models/User");
 const mongoose = require("mongoose");
@@ -95,6 +96,16 @@ describe("/api/returns", () => {
     const rentalInDb = await Rental.findById(rental._id);
     const diff = new Date() - rentalInDb.dateReturned;
     expect(diff).toBeLessThan(10 * 1000);
+  });
+
+  it("should set the rentalFee if input is valid", async () => {
+    rental.dateOut = moment().add(-7, "days").toDate();
+    await rental.save();
+
+    const res = await exec();
+
+    const rentalInDb = await Rental.findById(rental._id);
+    expect(rentalInDb.rentalFee).toBe(14);
   });
 
 });
