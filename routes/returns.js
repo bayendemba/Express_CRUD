f28876a9be031dcd48a159cf68683d8a27844712
bayendemba <bayendemba@gmail.com>
2018-11-27 17:33:24 +0000

    Looking up an object

--- routes/returns.js
+++ routes/returns.js
@@ -1,3 +1,4 @@
+const { Rental } = require("../models/Rental");
 const express = require("express");
 const router = express.Router();
 
@@ -5,6 +6,12 @@ router.post("/", async (req, res) => {
   if (!req.body.customerId) return res.status(400).send("customerId not provided");
   if (!req.body.movieId) return res.status(400).send("movieId not provided");
   
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if(!rental) return res.status(404).send("Rental not found");
+
   res.status(401).send("Unauthorized");
 });
 
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -66,4 +66,12 @@ describe("/api/returns", () => {
     expect(res.status).toBe(400);
   });
 
+  it("should return 404 if no rental found for the customer/movie", async () => {
+    Rental.remove({});
+
+    const res = await exec();
+
+    expect(res.status).toBe(404);
+  });
+
 });
